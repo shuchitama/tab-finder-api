0b1c1b464df066660e47fb4fa59344c67be2448b
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-14 15:11:45 -0700

    query returns an array of chords in the song

--- src/routes/songchords.js
+++ src/routes/songchords.js
@@ -1,16 +1,33 @@
 const router = require("express").Router();
 
 module.exports = db => {
-  router.get("/songchords", (request, response) => {
+  router.get("/songchords/:id", (request, response) => {
     db.query(
       `
-      SELECT
-        *
-      FROM song_chords
-    `
-    ).then(({ rows: songchords }) => {
-      response.json(songchords);
-    });
+      SELECT chord_id FROM song_chords WHERE song_id = $1;
+      `, [request.params.id]
+    )
+    .then(({ rows: chordsObj }) => {
+      let chordsArr = [];
+      for(const obj of chordsObj) {
+        chordsArr.push(obj.chord_id)
+      }
+      response.json(chordsArr)
+    })
+    .catch(err => console.log(err))
+
+
+    // db.query(
+    //   `
+    //   SELECT
+    //     *
+    //   FROM song_chords;
+    // `
+    // ).then(({ rows: songchords }) => {
+    //   response.json(songchords);
+    // });
+
+
   });
 
   return router;
