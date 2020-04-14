bc8d6131d41be033e3f3de705290db9b98d1102a
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-14 16:52:21 -0700

    tweak query to get songs

--- src/routes/songs.js
+++ src/routes/songs.js
@@ -4,11 +4,12 @@ module.exports = db => {
   router.get("/songs", (request, response) => {
     db.query(
       `
-      SELECT id, title, artist, string_agg(chords.name, ', ') as chords 
+      SELECT songs.id, title, artist, string_agg(chords.name, ', ') as chords 
       FROM songs 
       JOIN song_chords ON song_chords.song_id = songs.id 
       JOIN chords ON chords.id = song_chords.chord_id 
-      GROUP BY title, artist;
+      GROUP BY songs.id, title, artist
+      ORDER BY songs.id;
     `
     ).then(({ rows: songs }) => {
       response.json(songs);
