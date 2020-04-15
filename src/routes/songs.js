0a43de5dab51b57c34d4d3ee53951d5938d36ff2
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-04-14 18:34:09 -0700

    return song chords as array instead of string

--- src/routes/songs.js
+++ src/routes/songs.js
@@ -4,7 +4,7 @@ module.exports = db => {
   router.get("/songs", (request, response) => {
     db.query(
       `
-      SELECT songs.id, title, artist, string_agg(chords.name, ', ') as chords 
+      SELECT songs.id, title, artist, string_agg(chords.name, ',') as chords 
       FROM songs 
       JOIN song_chords ON song_chords.song_id = songs.id 
       JOIN chords ON chords.id = song_chords.chord_id 
@@ -12,6 +12,10 @@ module.exports = db => {
       ORDER BY songs.id;
     `
     ).then(({ rows: songs }) => {
+      for (const song of songs) {
+        // console.log(song);
+        song.chords = song.chords.split(',');
+      }
       response.json(songs);
     });
   });
