f4bbc932ae3b97c470d343f82926d829ac79457b
Shuchita <shuchita@live.com>
2021-03-31 22:08:45 -0700

    update db connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,17 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+	// connectionString: process.env.DATABASE_URL || "",
+	user: "development",
+	password: "development",
+	database: "db_development",
+	port: 5432,
+	host: "localhost",
+	ssl: true,
 });
 
 client
-  .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+	.connect()
+	.catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;
