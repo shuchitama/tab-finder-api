4ba26b685d46deb584e3f704e4138ddfe595826a
Shuchita <shuchita@live.com>
2021-03-31 22:32:44 -0700

    revert db changes

--- src/db/index.js
+++ src/db/index.js
@@ -2,12 +2,6 @@ const pg = require("pg");
 
 const client = new pg.Client({
 	connectionString: process.env.DATABASE_URL || "",
-	user: "development",
-	password: "development",
-	database: "db_development",
-	port: 5432,
-	host: "localhost",
-	ssl: true,
 });
 
 client
