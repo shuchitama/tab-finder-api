e185ea3878dd9af143b024d9eb911e69bad18869
Shuchita <shuchita@live.com>
2021-06-08 17:55:19 -0700

    change ssl settings

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,8 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-	// connectionString: process.env.DATABASE_URL || "",
-	user: "idmhnjdujakfvd",
-	password: "ac0cddfe1f00b69730225b3c94fd8a09372703e57aff53c911454873feda67d0",
-	database: "dbnoioou02pqnd",
-	port: 5432,
-	host: "ec2-52-45-73-150.compute-1.amazonaws.com",
+	connectionString: process.env.DATABASE_URL || "",
+	ssl: { rejectUnauthorized: false },
 });
 
 client
